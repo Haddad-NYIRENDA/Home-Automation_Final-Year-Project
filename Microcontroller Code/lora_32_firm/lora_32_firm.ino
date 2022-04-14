8c143581df68a10444a4e333e1f5c86749c099e5
Ramsey Ith Njema <bsc-03-16@unima.ac.mw>
2022-04-14 06:40:33 +0200

    Removed API key secret

--- Microcontroller Code/lora_32_firm/lora_32_firm.ino
+++ Microcontroller Code/lora_32_firm/lora_32_firm.ino
@@ -49,7 +49,7 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
  
 //For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 /* 2. Define the API Key */
-#define API_KEY "AIzaSyCocqUsUnVBTJ40_I1BOoNamgNpSQTnDnM"
+#define API_KEY " "
 /* 3. Define the RTDB URL */
 #define DATABASE_URL "https://shautom-app-test-default-rtdb.asia-southeast1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
